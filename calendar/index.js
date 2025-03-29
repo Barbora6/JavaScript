be554404a72aa90ba30d5280a7bad4802bbca3c2
Barbora6 <smetanovabarbora@email.cz>
2025-03-29 09:52:24 +0100

    java script kod

--- calendar/index.js
+++ calendar/index.js
@@ -1,4 +1,19 @@
 const monthNameEl = document.getElementById("month-name");
-const dayNameEl = document.getElementById("day-neme");
+const dayNameEl = document.getElementById("day-name");
 const dayNumEl = document.getElementById("day-number");
 const yearEl = document.getElementById("year");
+
+const date = new Date();
+const month = date.getMonth();
+
+monthNameEl.innerText = date.toLocaleString("cz", {
+  month: "long"
+});
+
+dayNameEl.innerText = date.toLocaleString("cz", {
+  weekday: "long"
+});
+
+dayNumEl.innerText = date.getDate();
+
+yearEl.innerText = date.getFullYear();
