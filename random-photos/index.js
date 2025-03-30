fe765d1a61c4900f168a294f185b7b0b070afa9e
Barbora6 <smetanovabarbora@email.cz>
2025-03-30 15:02:09 +0200

    generovani vice obrazků najednou

--- random-photos/index.js
+++ random-photos/index.js
@@ -1,14 +1,19 @@
+// projekt 5 - random images
+// https://picsum.photos/
 const imageContainerEl = document.querySelector(".image-container");
 const buttonEl = document.querySelector(".btn");
 
 buttonEl.addEventListener("click", () => {
+  imageNum = 5;
   addNewImages();
 });
 
 function addNewImages() {
-  const newImageEl = document.createElement("img");
-  newImageEl.src = `https://picsum.photos/300?random=${Math.floor(
-    Math.random() * 2000
-  )}`;
-  imageContainerEl.appendChild(newImageEl);
+  for (let index = 0; index < imageNum; index++) {
+    const newImageEl = document.createElement("img");
+    newImageEl.src = `https://picsum.photos/300?random=${Math.floor(
+      Math.random() * 2000
+    )}`;
+    imageContainerEl.appendChild(newImageEl);
+  }
 }
