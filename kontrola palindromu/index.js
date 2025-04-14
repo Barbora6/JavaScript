16a554876c98eaa21f411c7955dc53c8acd6316a
Barbora6 <smetanovabarbora@email.cz>
2025-04-14 21:32:18 +0200

    JS kod

--- kontrola palindromu/index.js
+++ kontrola palindromu/index.js
@@ -2,14 +2,23 @@ const input = document.getElementById("input");
 const btnInput = document.querySelector("#btn");
 
 function prevodTextu(slovo) {
-  "hello word"[("hello", "word")];
+  //   "hello word"[("hello", "word")];
   //   Funkce split() v JavaScriptu rozdělí řetězec (string) na pole (array) podle zadaného oddělovače.
-  return slovo.split("");
+  //   'hello' => ['h', 'e', 'l', 'l', 'o']
+  return slovo.split("").reverse().join("");
 }
 
 function zkontrolujInput() {
   const value = input.value;
-  alert(value);
+  const reverse = prevodTextu(value);
+
+  if (value === reverse) {
+    alert("P A L I N D R O M E");
+  } else {
+    alert("Zkus to znovu!");
+  }
+
+  input.value = "";
 }
 
 btnInput.addEventListener("click", zkontrolujInput);
