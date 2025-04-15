2cb6f8c5a72f9318444b626bf5cb72e4de46530b
Barbora6 <smetanovabarbora@email.cz>
2025-04-15 22:25:07 +0200

    2.projekt - kliknutí na tlačítko přidání a odebrání textu

--- more-projects-js/index.js
+++ more-projects-js/index.js
@@ -9,3 +9,19 @@ btnHi.addEventListener("click", () => {
   const yourName = prompt("Prosím, zadejte svoje jméno:");
   spanEl.textContent = yourName;
 });
+
+// 2.projekt:
+
+// kliknutím na tlačítko se schová text:
+
+const btnModal = document.querySelector("#openModal");
+const modalEl = document.querySelector(".modal");
+const modalContentEl = modalEl.querySelector(".modal_content");
+
+btnModal.addEventListener("click", () => {
+  modalEl.classList.add("open");
+});
+
+modalContentEl.addEventListener("click", () => {
+  modalEl.classList.remove("open");
+});
